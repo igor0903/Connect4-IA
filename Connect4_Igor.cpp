9513cbf0ffd45259d1ab7d38492f81888f10026c
Igor Melo <igor0903@live.com>
2022-04-20 11:59:22 +0100

    MINIMAX FUNCIONANDO

--- Connect4_Igor.cpp
+++ Connect4_Igor.cpp
@@ -5,8 +5,11 @@
 
 using namespace std;
 
+int coluna = -1;
 int jogadas = 0;
 int verific = 0;
+int verific_interno = 0;
+int numerodenos = 0;
 
 typedef struct node{
   char tabu[6][7];
@@ -14,6 +17,11 @@ typedef struct node{
   struct node *filhos[7];
   struct node *ant;
 } *Node;
+
+bool input(char c, int col, Node n);
+void input_interno(char c, int col, Node n);
+int minimize(Node n, int depth, bool c);
+int maximize(Node n, int depth, bool c);
 
 Node des(){
   Node aux = (Node)malloc(sizeof(struct node));
@@ -23,6 +31,39 @@ Node des(){
   }
 
   return aux;
+}
+
+Node mknode(Node n, int col, char c){
+  numerodenos++;
+  Node aux = (Node)malloc(sizeof(struct node));
+  
+  for(int i=0; i<6; i++){
+    for(int j=0; j<7; j++)
+      aux->tabu[i][j] = n->tabu[i][j];
+  }
+
+  input_interno(c,col,aux);
+  //aux->util = utility(aux);
+  aux->ant = n;
+  return aux;
+}
+
+bool valid_locations(Node n, int col){
+  bool localtion[7];
+  for(int i = 0; i < 7; i++){
+    if(n->tabu[0][i] == '-'){
+      localtion[i] = true;
+    }
+    else{
+      localtion[i] = false;
+    }
+  }
+  if(localtion[col] == true){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 int pontuacao(int x, int o){
@@ -306,14 +347,54 @@ void verif(int linha, int col, Node n){
   int ver2 = vertver(linha, col, n);
   int ver3 = diag1ver(linha,col, n);
   int ver4 = diag2ver(linha,col, n);
-    printf("verHor:%d verVertical:%d verDiag1:%d verDiag2:%d\n", ver1, ver2, ver3, ver4);
+    //printf("verHor:%d verVertical:%d verDiag1:%d verDiag2:%d\n", ver1, ver2, ver3, ver4);
     if(ver1 == 1 || ver2 == 1 || ver3 == 1 || ver4 == 1)
       verific = 1;
     else if(jogadas == 42)
       verific = -1;
 }
 
-void input(char c, int col, Node n){
+void verif_interno(int linha, int col, Node n){
+
+  int ver1 = horver(linha, col, n);
+  int ver2 = vertver(linha, col, n);
+  int ver3 = diag1ver(linha,col, n);
+  int ver4 = diag2ver(linha,col, n);
+    //printf("verHor:%d verVertical:%d verDiag1:%d verDiag2:%d\n", ver1, ver2, ver3, ver4);
+    if(ver1 == 1 || ver2 == 1 || ver3 == 1 || ver4 == 1)
+      verific_interno = 1;
+    else if(jogadas == 42)
+      verific_interno = -1;
+}
+
+bool input(char c, int col, Node n){
+  if(valid_locations(n, col)){
+    int linha = 5;
+    while(n->tabu[linha][col] != '-'){ // procura o proximo lugar livre da coluna
+      linha--;
+    }
+    n->tabu[linha][col] = c;
+    printt(n);
+    verif(linha, col, n);
+    return true;
+  }
+  else{
+    cout << "lugar invalido" << endl;
+    return false;
+  }
+
+}
+
+void input_interno(char c, int col, Node n){
+  int linha = 5;
+  while(n->tabu[linha][col] != '-'){ // procura o proximo lugar livre da coluna
+    linha--;
+  }
+  n->tabu[linha][col] = c;
+  //verif(linha, col, n);
+}
+
+void input2(char c, int col, Node n){
   int linha = 5;
   while(n->tabu[linha][col] != '-'){ // procura o proximo lugar livre da coluna
     linha--;
@@ -323,6 +404,63 @@ void input(char c, int col, Node n){
   verif(linha, col, n);
 }
 
+int minimax(Node n, int depth, bool c){
+  int value;
+  int new_score;
+  value = -99999999;
+  for(int i = 0; i < 7; i++){
+    if(valid_locations(n, i)){
+      n->filhos[i] = mknode(n, i, 'X');
+      new_score = maximize(n->filhos[i], depth -1, false);
+      if(new_score > value){
+        value = new_score;
+        coluna = i;
+      }
+    }
+  }
+  return coluna;
+}
+int maximize(Node n, int depth, bool c){
+  int value;
+  int new_score;
+  verif_interno(6,7,n);
+  if(depth == 0 || verific_interno == 1 || verific_interno == -1){
+    return utility(n);
+  } //MAXIMIZAR O JOGADOR X
+  value = -99999999;
+  for(int i = 0; i < 7; i++){
+    if(valid_locations(n, i)){
+      n->filhos[i] = mknode(n, i, 'X');
+      new_score = minimize(n->filhos[i], depth -1, false);
+      if(new_score > value){
+        value = new_score;
+      }
+    }
+  }
+  return value;
+}
+
+int minimize(Node n, int depth, bool c){
+  int value;
+  int new_score;
+  verif_interno(6,7,n);
+  if(depth == 0 || verific_interno == 1 || verific_interno == -1){
+    return utility(n);
+  }
+  else{
+    value = 99999999;
+    for(int i = 0; i < 7; i++){
+      if(valid_locations(n, i)){
+        n->filhos[i] = mknode(n, i, 'O');
+        new_score = maximize(n->filhos[i], depth -1, true);
+        if(new_score < value){
+          value = new_score;
+        }
+      }
+    }
+    return value;
+  }
+}
 
 
 int main(int argc, char **argv){
@@ -330,17 +468,18 @@ int main(int argc, char **argv){
   Node tab1 = des();
   printt(tab1);
 
-  int random = 0;
-  int erro_input = 0;
+  //int random = 0;
   //int i=0;
   int jog;
   int vez = 1;
   while(jogadas < 42 && verific == 0){
     if(vez == 1){
-      cout << "Jogador 1 (X)" << endl; // endl = \n
-      cin >> jog;
+      //cin >> jog;
+      jog = minimax(tab1, 3, true); //profundidade 3 ja apresentar um bom nivel de resposta da IA para equilibrar tempo e qualidade da resposta
+      cout << jog << endl;
       jogadas++;
-      input('X', jog-1, tab1);
+      input2('X', jog, tab1);
+      verif(6,7,tab1);
       printf("Score Colunas: %d\n", horutil(tab1));
       printf("Score Linhas: %d\n", verutil(tab1));
       printf("Score diagonal 1: %d\n", diagMainUtil(tab1));
@@ -348,17 +487,22 @@ int main(int argc, char **argv){
       cout<<"Utilty: " << utility(tab1) << endl;
     }
     if(vez == 2){
-      cout << "IA (O)" << endl;
-      srand (time(NULL));
-      random=rand() % 7 + 1; //number entre 1 e 7
-      jog = random;
+      cout << "Sua vez, Jogador (O)" << endl;
+      //srand (time(NULL));
+      //random=rand() % 7 + 1; //number entre 1 e 7
+      //jog = minimax(tab1);
+      cin >> jog;
       jogadas++;
-      input('O', jog-1, tab1);
+      while(!input('O', jog-1, tab1)){
+        cin >> jog;
+      }
+      verif(6,7,tab1);
       printf("Score Colunas: %d\n", horutil(tab1));
       printf("Score Linhas: %d\n", verutil(tab1));
       printf("Score diagonal 1: %d\n", diagMainUtil(tab1));
       printf("Score diagonal 2: %d\n", antiDiagUtil(tab1));
       cout<<"Utilty: " << utility(tab1) << endl;
+      //cout<<"minimax: " << minimax(tab1) << endl;
     }
     if(vez == 2)
       vez = 1;
@@ -366,15 +510,13 @@ int main(int argc, char **argv){
       vez = 2;
     printf("jogadas:%d\n", jogadas);
   }
-  
-  if(verific == -1)
-    printf("Empate\n");
-  else{
-    if(jogadas%2 == 0)
-      printf("Jogador 2 ganhou\n");
-    else
-      printf("Jogador 1 ganhou\n");
-  }
-
+    if(verific == -1)
+      printf("Empate\n");
+    else{
+      if(jogadas%2 == 0)
+       printf("Jogador ganhou\n");
+     else
+       printf("AI ganhou\n");
+    }
   return 0;
 }
